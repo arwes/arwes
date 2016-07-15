c40e021ed0415bdbdb0c17f7295dd853af5d0859
Romel Pérez <ronelprhone@gmail.com>
2016-07-15 17:56:31 -0500

    fix: loading

--- src/js/components/loading.js
+++ src/js/components/loading.js
@@ -16,9 +16,11 @@ const Loading = React.createClass({
     const cls = `pr-loading ${status} ${done}`;
     return (
       <div className={cls}>
-        <div className='pr-loading__content'>
-          <div className='pr-loading__circle1' />
-          <div className='pr-loading__circle2' />
+        <div className='pr-loading__frame'>
+          <div className='pr-loading__content'>
+            <div className='pr-loading__circle1' />
+            <div className='pr-loading__circle2' />
+          </div>
         </div>
       </div>
     );
