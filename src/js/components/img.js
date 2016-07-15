881055d4412f304b727d1fa8e7fbeb0ea64fa70b
Romel Pérez <ronelprhone@gmail.com>
2016-07-15 14:30:29 -0500

    fix: animations, img box

--- src/js/components/img.js
+++ src/js/components/img.js
@@ -1,10 +1,13 @@
 const React = require('react');
+const Box = require('./box');
 
 const Img = React.createClass({
 
   getDefaultProps () {
     return {
-      src: ''
+      src: '',
+      alt: null,
+      small: false
     };
   },
 
@@ -12,8 +15,12 @@ const Img = React.createClass({
     const props = this.props;
     return (
       <div className='pr-img'>
-        <div className='pr-img__cover'></div>
-        <img src={props.src} />
+        <Box small={props.small}>
+          <div className='pr-img__frame'>
+            <div className='pr-img__cover'></div>
+            <img src={props.src} alt={props.alt} />
+          </div>
+        </Box>
       </div>
     );
   }
