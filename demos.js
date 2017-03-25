dafa9cb289faf886c68b58a9735b1647a8a66fb7
Romel Pérez <ronelprhone@gmail.com>
2017-03-25 16:08:13 -0500

    [chore] utils

--- demos.js
+++ demos.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 7000;
 
 app.use(express.static(`${process.cwd()}/demos`));
+app.use(express.static(`${process.cwd()}/dist`));
 
 app.listen(port, function (err) {
   if (err) throw err;
