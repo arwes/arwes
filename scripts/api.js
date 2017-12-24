7aa812980354fe5ebc879542661a3fe91ca14f4c
Romel Pérez <ronelprhone@gmail.com>
2017-12-24 00:36:01 -0500

    [chore] add script to generate components api

---
+++ scripts/api.js
@@ -0,0 +1,57 @@
+// Generate the components API documentation.
+// For each component, its source code and readme files are read and parsed,
+// then its docs config is saved in a file and its next.js page is created.
+
+const fs = require('fs');
+const path = require('path');
+const docgen = require('react-docgen');
+
+const { components } = require('../site/settings');
+const componentsPath = './site/api-components';
+const docsPath = './pages/api';
+const readmeFileName = 'Readme.md';
+const encoding = 'utf8';
+
+components.
+  filter(name => !!name).
+  map(name => {
+
+    const folderPath = 'src/' + name;
+    const nameLower = name.toLowerCase();
+
+    const componentPath = path.resolve(process.cwd(), folderPath, name + '.js');
+    const componentSrc = fs.readFileSync(componentPath, { encoding });
+    const api = docgen.parse(componentSrc);
+
+    const readmePath = path.resolve(process.cwd(), folderPath, readmeFileName);
+    const readme = fs.readFileSync(readmePath, { encoding });
+
+    // Generate the component defintion content to import in the page.
+    const componentContent = JSON.stringify({
+      name,
+      path: folderPath,
+      api,
+      readme
+    });
+
+    fs.writeFileSync(
+      path.resolve(componentsPath, nameLower + '.json'),
+      componentContent,
+      { encoding }
+    );
+
+    // Generate the page component content.
+    const pageContent = `import withApi from '../../site/withApi';
+import component from '../../site/api-components/${nameLower}.json';
+
+export default withApi({ component });
+`;
+
+    fs.writeFileSync(
+      path.resolve(docsPath, nameLower + '.js'),
+      pageContent,
+      { encoding }
+    );
+  });
+
+console.log(components.length + ' components docs were generated.');
