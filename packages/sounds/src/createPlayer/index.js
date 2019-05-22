25b22d34e3fd4d4210f069c9e4e5b06d8772a782
Romel Pérez <ronelprhone@gmail.com>
2019-05-21 22:09:13 -0500

    chore: upgrade sounds dependencies

--- packages/sounds/src/createPlayer/index.js
+++ packages/sounds/src/createPlayer/index.js
@@ -1,7 +1,7 @@
-import howler from 'howler';
+import { Howl } from 'howler/dist/howler.core.min.js';
 
 import makeCreatePlayer from './makeCreatePlayer';
 
-const createPlayer = makeCreatePlayer({ Howl: howler.Howl });
+const createPlayer = makeCreatePlayer({ Howl });
 
 export default createPlayer;
