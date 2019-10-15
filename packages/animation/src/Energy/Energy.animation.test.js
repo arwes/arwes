ac9ab159ee520a163b75a3abf3d1b3816222729f
Romel Pérez <ronelprhone@gmail.com>
2019-10-14 22:07:42 -0500

    feat: Energy props

--- packages/animation/src/Energy/Energy.animation.test.js
+++ packages/animation/src/Energy/Energy.animation.test.js
@@ -22,6 +22,46 @@ test('Should set energy flow as "entered" if no "animate"', () => {
   expect(energy.state.flow.entered).toBe(true);
 });
 
+test('Should delay energy flow from "exited" the "duration.delay" time', () => {
+  let energy;
+  render(<Energy ref={r => (energy = r)} duration={{ enter: 100, delay: 100 }} />);
+  expect(energy.state.flow.exited).toBe(true);
+  jest.advanceTimersByTime(90);
+  expect(energy.state.flow.exited).toBe(true);
+  jest.advanceTimersByTime(20); // 110ms
+  expect(energy.state.flow.entering).toBe(true);
+});
+
+test('Should get notified with "onActivate" when activation changes', () => {
+  let example;
+  const onActivate = jest.fn();
+  class Example extends React.PureComponent {
+    state = { activate: true }
+    render () {
+      const { activate } = this.state;
+      return <Energy activate={activate} duration={100} onActivate={onActivate} />;
+    }
+  }
+  render(<Example ref={r => (example = r)} />);
+  setTimeout(() => example.setState({ activate: false }), 200);
+
+  expect(onActivate).not.toHaveBeenCalled();
+
+  jest.advanceTimersByTime(10);
+  expect(onActivate).toHaveBeenCalledTimes(1);
+  expect(onActivate).toHaveBeenCalledWith(true);
+
+  jest.advanceTimersByTime(180); // 190ms
+  expect(onActivate).toHaveBeenCalledTimes(1);
+
+  jest.advanceTimersByTime(20); // 210ms
+  expect(onActivate).toHaveBeenCalledTimes(2);
+  expect(onActivate).toHaveBeenCalledWith(false);
+
+  jest.advanceTimersByTime(100); // 310ms
+  expect(onActivate).toHaveBeenCalledTimes(2);
+});
+
 describe('root', () => {
   test('Should set energy flow as "exited" if "activate", and start "entering"', () => {
     let energy;
@@ -120,6 +160,41 @@ describe('parent energy', () => {
     jest.advanceTimersByTime(200); // 510ms
     expect(energy.state.flow.exiting).toBe(true);
     jest.advanceTimersByTime(100); // 610ms
+    expect(energy.state.flow.exited).toBe(true);
+  });
+
+  test('Should parent energy context set energy flow to enter if "entering" or "entered" if "merge=true"', () => {
+    let example;
+    let energy;
+    class Example extends React.PureComponent {
+      state = { flow: { exited: true } }
+      render () {
+        const { flow } = this.state;
+        return (
+          <EnergyContext.Provider value={{ flow }}>
+            <Energy ref={r => (energy = r)} duration={100} merge />
+          </EnergyContext.Provider>
+        );
+      }
+    }
+    render(<Example ref={r => (example = r)} />);
+    setTimeout(() => example.setState({ flow: { entering: true } }), 100);
+    setTimeout(() => example.setState({ flow: { entered: true } }), 200);
+    setTimeout(() => example.setState({ flow: { exiting: true } }), 400);
+    setTimeout(() => example.setState({ flow: { exited: true } }), 500);
+
+    jest.advanceTimersByTime(10); // 10ms
+    expect(energy.state.flow.exited).toBe(true);
+
+    jest.advanceTimersByTime(100); // 110ms
+    expect(energy.state.flow.entering).toBe(true);
+    jest.advanceTimersByTime(100); // 210ms
+    expect(energy.state.flow.entered).toBe(true);
+
+    // It should be "exiting" when its parent is "exiting".
+    jest.advanceTimersByTime(200); // 410ms
+    expect(energy.state.flow.exiting).toBe(true);
+    jest.advanceTimersByTime(100); // 510ms
     expect(energy.state.flow.exited).toBe(true);
   });
 });
