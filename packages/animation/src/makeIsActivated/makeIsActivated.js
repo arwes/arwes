999a139bab63413497548555e41bef899467f03d
Romel Pérez <ronelprhone@gmail.com>
2020-01-11 22:42:19 -0500

    feat: animation makeIsAnimated outsourced support

--- packages/animation/src/makeIsActivated/makeIsActivated.js
+++ packages/animation/src/makeIsActivated/makeIsActivated.js
@@ -1,20 +1,32 @@
+import { ENTERING, ENTERED } from '../constants';
+
 function makeIsActivated (component) {
   return function isActivated () {
+    // Is root
     if (component.isRoot()) {
       if (component.props.activate !== void 0) {
         return component.props.activate;
       }
+
       return true;
     }
 
-    const parentFlow = component.props.parentEnergyContext.flow;
+    // Is controlled child
+    else if (component.props.parentEnergyContext) {
+      const parentFlow = component.props.parentEnergyContext.flow;
 
-    if (component.props.merge) {
-      return !!(parentFlow.entering || parentFlow.entered);
-    }
-    else {
+      if (component.props.merge) {
+        return !!(parentFlow.entering || parentFlow.entered);
+      }
+
       return !!parentFlow.entered;
     }
+
+    // Is outsourced
+
+    const { flowValue } = component.state;
+
+    return flowValue === ENTERING || flowValue === ENTERED;
   };
 }
 
--- packages/animation/src/makeIsActivated/makeIsActivated.test.js
+++ packages/animation/src/makeIsActivated/makeIsActivated.test.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 
+import { ENTERING, ENTERED, EXITING, EXITED } from '../constants';
 import { makeIsActivated } from './makeIsActivated';
 
 test('Should return true if root by default', () => {
@@ -42,4 +43,18 @@ test('Should return false if parent energy flow is not "entered"', () => {
   const component = { props: { parentEnergyContext: { flow: {} } }, isRoot };
   const isActivated = makeIsActivated(component);
   expect(isActivated()).toBe(false);
+});
+
+[
+  { flowValue: ENTERING, expected: true },
+  { flowValue: ENTERED, expected: true },
+  { flowValue: EXITING, expected: false },
+  { flowValue: EXITED, expected: false }
+].forEach(({ flowValue, expected }) => {
+  test(`Should return ${expected} if "${flowValue}", not root, not parent energy`, () => {
+    const isRoot = jest.fn(() => false);
+    const component = { state: { flowValue }, props: {}, isRoot };
+    const isActivated = makeIsActivated(component);
+    expect(isActivated()).toBe(expected);
+  });
 });
