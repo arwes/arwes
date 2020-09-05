fb16bc0dc8200790999d1ee8a65db9f6a3b3e3fa
Romel Pérez <ronelprhone@gmail.com>
2020-09-05 00:16:44 -0500

    chore: setup files

--- compile.sh
+++ compile.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Compile all the specified packages JavaScript source code with Babel.
-# They compile from "packages/[name]/src" to "packages/[name]/lib".
+# They compile from "packages/[package]/src" to "packages/[package]/lib".
 
 alias babel='./node_modules/.bin/babel'
 
-log() {
+log () {
   echo "\033[1;36m$1\033[0m"
 }
 
-build() {
+build () {
   log "\nCompiling $1..."
   NODE_ENV=production
   rm -rf ./packages/$1/lib/
