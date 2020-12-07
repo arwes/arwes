bf669d5606f8ea96dcb325441a4eca307993a02b
Romel Pérez <ronelprhone@gmail.com>
2020-12-07 17:01:08 -0500

    fix(animation): add constant objects freeze

--- packages/animation/src/constants.js
+++ packages/animation/src/constants.js
@@ -3,10 +3,10 @@ export const ENTERED = 'entered';
 export const EXITING = 'exiting';
 export const EXITED = 'exited';
 export const ANIMATE_DEFAULT = true;
-export const DURATION_DEFAULT = {
+export const DURATION_DEFAULT = Object.freeze({
   enter: 100,
   exit: 100,
   stagger: 25,
   delay: 0,
   offset: 0
-};
+});
