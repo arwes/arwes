2d4f8195387b91fe118ebf055f5198528931c0db
Joshua Daniel Pratt Nielsen <jdpnielsen@gmail.com>
2020-12-24 02:29:05 +0100

    fix(animation): improve typing of extendAnimator HOC

--- packages/animation/src/extendAnimator/extendAnimator.ts
+++ packages/animation/src/extendAnimator/extendAnimator.ts
@@ -1,25 +1,25 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-import { ComponentType, Ref, createElement, forwardRef } from 'react';
+import { ComponentType, createElement, forwardRef, Component, FC } from 'react';
 
-import { AnimatorClassSettings, AnimatorInstanceSettings } from '../constants';
+import { AnimatorInstanceSettings } from '../constants';
 import { mergeClassAndInstanceAnimatorSettings } from '../utils/mergeClassAndInstanceAnimatorSettings';
 
 interface ExtendAnimatorProps {
   animator?: AnimatorInstanceSettings
 }
 
-function extendAnimator (extendedClassAnimator: AnimatorClassSettings) {
-  const extendAnimatorWrapper = <T extends ExtendAnimatorProps>(InputComponent: ComponentType<T>) => {
-    const OutputComponent = forwardRef<Ref<any>, T>((props, ref) => {
+function extendAnimator<T extends (Component<P> | FC<P>), P extends ExtendAnimatorProps = ExtendAnimatorProps> (extendedClassAnimator: AnimatorInstanceSettings) {
+  const extendAnimatorWrapper = (InputComponent: ComponentType<P>) => {
+    const OutputComponent = forwardRef<T, P>((props, ref) => {
       const { animator: instanceAnimator, ...otherProps } = props;
       const resultAnimator = mergeClassAndInstanceAnimatorSettings(
         extendedClassAnimator,
         instanceAnimator
       );
 
-      return createElement(InputComponent, {
-        ...(otherProps as T),
+      return createElement<P>(InputComponent, {
+        ...(otherProps as P),
         animator: resultAnimator,
         ref
       });
