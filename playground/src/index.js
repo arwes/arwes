c1d09a4f6deca7bb87ea7b78c09da5f6a0216a30
Romel Pérez <ronelprhone@gmail.com>
2021-01-30 22:54:15 -0500

    chore(playground): disable anime suspend when document hidden

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import anime from 'animejs';
 
 import './ga';
 import { RouterControlsProvider } from './components/RouterControlsProvider';
 import { App } from './components/App';
+
+anime.suspendWhenDocumentHidden = false;
 
 render(
   <BrowserRouter>
