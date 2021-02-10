33bb33e591505c96d8582d6b6cca735d77631335
Romel Pérez <ronelprhone@gmail.com>
2021-02-10 13:57:15 -0500

    chore: husky settings

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+husky install
+
 # If in continuous integration, the npm "ci" script is going to take care of the
-# the project setup completely, so the postinstall setup would be redundant.
+# the project setup completely, so the postinstall bootstrap and compilation
+# would be redundant.
 
 if [ -z $CI ] || [ $CI != "true" ]
 then
