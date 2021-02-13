6358972b62273fd26617f2905a53352930aec973
Romel Pérez <ronelprhone@gmail.com>
2021-02-12 22:45:08 -0500

    fix(core): text bleep category

--- packages/core/src/Text/Text.bleeps.ts
+++ packages/core/src/Text/Text.bleeps.ts
@@ -2,7 +2,8 @@ import { BleepsSettings } from '@arwes/sounds';
 
 const bleepsSettings: BleepsSettings = {
   typing: {
-    player: 'typing'
+    player: 'typing',
+    category: 'transition'
   }
 };
 
