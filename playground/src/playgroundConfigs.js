01363cc33f1ac42ac1870066f2eceeb14539db61
Romel Pérez <ronelprhone@gmail.com>
2021-02-17 10:27:11 -0500

    chore: rename components sandboxes files

--- playground/src/playgroundConfigs.js
+++ playground/src/playgroundConfigs.js
@@ -5,8 +5,8 @@ const playgroundConfigs = [
       {
         name: 'createTheme',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/design/src/createTheme/basic.sandbox.md').default },
-          { name: 'dynamic', code: require('repository/packages/design/src/createTheme/dynamic.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/design/src/createTheme/createTheme.basic.sandbox.md').default },
+          { name: 'dynamic', code: require('repository/packages/design/src/createTheme/createTheme.dynamic.sandbox.md').default }
         ]
       }
     ]
@@ -17,23 +17,23 @@ const playgroundConfigs = [
       {
         name: 'withAnimator',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/animation/src/withAnimator/basic.sandbox.md').default },
-          { name: 'nesting', code: require('repository/packages/animation/src/withAnimator/nesting.sandbox.md').default },
-          { name: 'staggering', code: require('repository/packages/animation/src/withAnimator/staggering.sandbox.md').default },
-          { name: 'sequence', code: require('repository/packages/animation/src/withAnimator/sequence.sandbox.md').default },
-          { name: 'custom-manager', code: require('repository/packages/animation/src/withAnimator/custom-manager.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/animation/src/withAnimator/withAnimator.basic.sandbox.md').default },
+          { name: 'nesting', code: require('repository/packages/animation/src/withAnimator/withAnimator.nesting.sandbox.md').default },
+          { name: 'staggering', code: require('repository/packages/animation/src/withAnimator/withAnimator.staggering.sandbox.md').default },
+          { name: 'sequence', code: require('repository/packages/animation/src/withAnimator/withAnimator.sequence.sandbox.md').default },
+          { name: 'custom-manager', code: require('repository/packages/animation/src/withAnimator/withAnimator.custom-manager.sandbox.md').default }
         ]
       },
       {
         name: 'extendAnimator',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/animation/src/extendAnimator/basic.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/animation/src/extendAnimator/extendAnimator.basic.sandbox.md').default }
         ]
       },
       {
         name: 'Animator',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/animation/src/Animator/basic.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/animation/src/Animator/Animator.basic.sandbox.md').default }
         ]
       }
     ]
@@ -44,10 +44,10 @@ const playgroundConfigs = [
       {
         name: 'withBleeps',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/sounds/src/withBleeps/basic.sandbox.md').default },
-          { name: 'categories', code: require('repository/packages/sounds/src/withBleeps/categories.sandbox.md').default },
-          { name: 'loops', code: require('repository/packages/sounds/src/withBleeps/loops.sandbox.md').default },
-          { name: 'dynamic', code: require('repository/packages/sounds/src/withBleeps/dynamic.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/sounds/src/withBleeps/withBleeps.basic.sandbox.md').default },
+          { name: 'categories', code: require('repository/packages/sounds/src/withBleeps/withBleeps.categories.sandbox.md').default },
+          { name: 'loops', code: require('repository/packages/sounds/src/withBleeps/withBleeps.loops.sandbox.md').default },
+          { name: 'dynamic', code: require('repository/packages/sounds/src/withBleeps/withBleeps.dynamic.sandbox.md').default }
         ]
       }
     ]
@@ -58,31 +58,31 @@ const playgroundConfigs = [
       {
         name: 'StylesBaseline',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/core/src/StylesBaseline/basic.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/core/src/StylesBaseline/StylesBaseline.basic.sandbox.md').default }
         ]
       },
       {
         name: 'Text',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/core/src/Text/basic.sandbox.md').default },
-          { name: 'multiple', code: require('repository/packages/core/src/Text/multiple.sandbox.md').default },
-          { name: 'dynamic', code: require('repository/packages/core/src/Text/dynamic.sandbox.md').default },
-          { name: 'intercepting', code: require('repository/packages/core/src/Text/intercepting.sandbox.md').default },
-          { name: 'static', code: require('repository/packages/core/src/Text/static.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/core/src/Text/Text.basic.sandbox.md').default },
+          { name: 'multiple', code: require('repository/packages/core/src/Text/Text.multiple.sandbox.md').default },
+          { name: 'dynamic', code: require('repository/packages/core/src/Text/Text.dynamic.sandbox.md').default },
+          { name: 'intercepting', code: require('repository/packages/core/src/Text/Text.intercepting.sandbox.md').default },
+          { name: 'static', code: require('repository/packages/core/src/Text/Text.static.sandbox.md').default }
         ]
       },
       {
         name: 'Table',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/core/src/Table/basic.sandbox.md').default },
-          { name: 'static', code: require('repository/packages/core/src/Table/static.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/core/src/Table/Table.basic.sandbox.md').default },
+          { name: 'static', code: require('repository/packages/core/src/Table/Table.static.sandbox.md').default }
         ]
       },
       {
         name: 'CodeBlock',
         sandboxes: [
-          { name: 'basic', code: require('repository/packages/core/src/CodeBlock/basic.sandbox.md').default },
-          { name: 'static', code: require('repository/packages/core/src/CodeBlock/static.sandbox.md').default }
+          { name: 'basic', code: require('repository/packages/core/src/CodeBlock/CodeBlock.basic.sandbox.md').default },
+          { name: 'static', code: require('repository/packages/core/src/CodeBlock/CodeBlock.static.sandbox.md').default }
         ]
       }
     ]
