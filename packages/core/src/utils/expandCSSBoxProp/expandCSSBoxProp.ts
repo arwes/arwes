5c322aec3676eb5ebb465900d2c45697e3356b84
Romel Pérez <ronelprhone@gmail.com>
2021-04-08 18:26:52 -0500

    feat(core): expand css box prop utility

---
+++ packages/core/src/utils/expandCSSBoxProp/expandCSSBoxProp.ts
@@ -0,0 +1,40 @@
+function expandCSSBoxProp <T> (value: T | T[] | undefined, defaultValue: T): T[] {
+  type P = T | undefined;
+  let expansion: P[] = [];
+
+  if (Array.isArray(value)) {
+    switch (value.length) {
+      case 1: {
+        const data = value[0];
+        expansion = [data, data, data, data];
+        break;
+      }
+      case 2: {
+        const [y, x] = value;
+        expansion = [y, x, y, x];
+        break;
+      }
+      case 3: {
+        const [top, x, bottom] = value;
+        expansion = [top, x, bottom, x];
+        break;
+      }
+    }
+
+    if (value.length >= 4) {
+      expansion = value;
+    }
+  }
+  else {
+    expansion = [value, value, value, value];
+  }
+
+  return [
+    expansion[0] ?? defaultValue,
+    expansion[1] ?? defaultValue,
+    expansion[2] ?? defaultValue,
+    expansion[3] ?? defaultValue
+  ];
+};
+
+export { expandCSSBoxProp };
