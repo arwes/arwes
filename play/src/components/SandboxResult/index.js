251c30c90c9e4e236c102f0cc1294d400b7091c0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 15:43:01 +0800

    Delete play directory

--- play/src/components/SandboxResult/index.js
+++
@@ -1,6 +0,0 @@
-import { withLive } from 'react-live';
-import { SandboxResult as Component } from './SandboxResult.component';
-
-const SandboxResult = withLive(Component);
-
-export { SandboxResult };
--- play/src/settings.js
+++
@@ -1,1 +0,0 @@
-export const GA_TRACKING_ID = 'UA-50433259-2';
