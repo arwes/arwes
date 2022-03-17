ea26f9cf384f3dd16693fdf554c5b64e4835d34f
Romel Perez <ronelprhone@gmail.com>
2022-03-17 00:42:25 -0500

    build: packages esm distribution files

--- scripts/pkg-build-esm.sh
+++ scripts/pkg-build-esm.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 mkdir -p ./build/esm
+echo "{ \"type\": \"module\" }" >| ./build/esm/package.json
 npx tsc -p ./tsconfig.build.esm.json $1
--- scripts/pkg-clean.sh
+++ scripts/pkg-clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -rf ./build
-rm .tsbuildinfo
+rm -f .tsbuildinfo
