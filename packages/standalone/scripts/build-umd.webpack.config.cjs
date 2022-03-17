77401735319483e3ad0d9571945add4ae6972378
Romel Perez <ronelprhone@gmail.com>
2022-03-16 20:34:27 -0500

    feat(standalone): package

---
+++ packages/standalone/scripts/build-umd.webpack.config.cjs
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const { NODE_ENV } = process.env;
+const CWD = process.cwd();
+const MOCKS = path.join(__dirname, '../../../scripts/pkg-build-umd-mocks');
+
+const mode = NODE_ENV || 'development';
+const outFilename = mode === 'production' ? 'arwes.min.js' : 'arwes.js';
+
+module.exports = {
+  mode,
+  entry: './src/index.ts',
+  output: {
+    path: path.join(CWD, './build/umd'),
+    filename: outFilename,
+    library: 'arwes',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      react$: path.join(MOCKS, 'react.js'),
+      '@emotion/react$': path.join(MOCKS, 'emotion/react.js'),
+      motion$: path.join(MOCKS, 'motion.js')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.build.umd.json'
+          }
+        }
+      }
+    ]
+  },
+  devtool: false
+};
