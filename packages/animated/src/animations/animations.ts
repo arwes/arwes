87a7b5017e6da5d946722be6f2f6ff39b19f7cbf
Romel Perez <ronelprhone@gmail.com>
2022-04-05 03:06:09 -0500

    feat(animated): aaVisibility

--- packages/animated/src/animations/animations.ts
+++ packages/animated/src/animations/animations.ts
@@ -1,3 +1,5 @@
+import { timeline } from 'motion';
+
 import type { AnimatedSettings } from '../types';
 
 const aaProperty = (prop: string, from: number | string, to: number | string): AnimatedSettings => ({
@@ -10,4 +12,28 @@ const aaProperty = (prop: string, from: number | string, to: number | string): A
 
 const aaOpacity = (): AnimatedSettings => aaProperty('opacity', 0, 1);
 
-export { aaProperty, aaOpacity };
+const aaVisibility = (): AnimatedSettings => {
+  return {
+    initialStyle: { opacity: 0 },
+    transitions: {
+      entering: ({ element, duration }) => timeline(
+        [
+          [element, { opacity: [0, 1] }],
+          [element, { opacity: [1, 0.6] }],
+          [element, { opacity: [0.6, 1] }]
+        ],
+        { duration }
+      ),
+      exiting: ({ element, duration }) => timeline(
+        [
+          [element, { opacity: [1, 0] }],
+          [element, { opacity: [0, 0.4] }],
+          [element, { opacity: [0.4, 0] }]
+        ],
+        { duration }
+      )
+    }
+  };
+};
+
+export { aaProperty, aaOpacity, aaVisibility };
