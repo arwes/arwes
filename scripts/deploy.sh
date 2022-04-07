061edfedd60a2978ddcc639f1e9bb64495490b0c
Romel Perez <ronelprhone@gmail.com>
2022-04-06 23:07:46 -0500

    ci: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,28 +2,30 @@
 
 if [ -z $1 ]
 then
-  echo "[ERROR] Please specify git branch."
+  echo "\n[ERROR] Please specify git branch."
   exit 1
 fi
 
-echo "[DEBUG] Fetching updates..."
+echo "\n[DEBUG] Fetching updates..."
 
 git fetch origin $1
 git reset --hard origin/$1
 
-echo "[DEBUG] Installing dependencies..."
+echo "\n[DEBUG] Installing dependencies..."
 
-npx cross-env NODE_ENV=production npm install
+npx npm install
 
-echo "[DEBUG] Building project..."
+echo "\n[DEBUG] Building project..."
 
 npx cross-env NODE_ENV=production npx nx run-many --target=build --all
 
-echo "[DEBUG] Creating www bundle..."
+echo "\n[DEBUG] Creating www bundle..."
 
+rm -rf ./www
 mkdir -p ./www
-cp -r ./apps/website/build/ ./www
+cp -r ./apps/static/* ./www
+cp -r ./apps/website/out/* ./www
 cp -r ./apps/play/build/play/ ./www
 cp -r ./apps/perf/build/perf/ ./www
 
-echo "[SUCCESS] Project deployed."
+echo "\n[SUCCESS] Project deployed."
