338b0a045b8154fcb766cc3af4ca75db6d772570
Romel Perez <ronelprhone@gmail.com>
2022-04-19 01:24:50 -0500

    ci: www build script

---
+++ scripts/www-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "\n[DEBUG] Installing dependencies..."
+
+npm install
+
+echo "\n[DEBUG] Building project..."
+
+npx cross-env NODE_ENV=production npx nx run-many --target build --all
+
+echo "\n[DEBUG] Creating www bundle..."
+
+rm -rf ./www
+mkdir -p ./www
+cp -r ./static/* ./www
+cp -r ./apps/website/out/* ./www
+cp -r ./apps/play/build/play/ ./www/play/
+cp -r ./apps/perf/build/perf/ ./www/perf/
+
+echo "\n[SUCCESS] Built successfully."
