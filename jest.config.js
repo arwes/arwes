b098a56f0d709d628e3fee09842da5495be559d5
Romel Perez <ronelprhone@gmail.com>
2022-04-19 01:17:31 -0500

    chore: jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,20 @@
 const glob = require('glob');
 
-const testMatch = glob
-  .sync('./packages/*')
-  .map(path => path.replace(/^\./, '<rootDir>'))
-  .map(path => path + '/src/**/*.test.ts?(x)');
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   verbose: true,
   testURL: 'http://localhost/',
-  testMatch
+  testMatch: glob
+    .sync('./packages/*')
+    .map(path => path.replace(/^\./, '<rootDir>'))
+    .map(path => path + '/src/**/*.test.ts?(x)'),
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/static/',
+    '/build/',
+    '/out/',
+    '/public/',
+    '/__deprecated__/'
+  ]
 };
