d7c16f921e007258f8a579498dcde0c3c817d8e4
Romel Perez <ronelprhone@gmail.com>
2022-04-23 19:32:55 -0500

    feat(styles): hooks

--- packages/styles/src/types.ts
+++ packages/styles/src/types.ts
@@ -4,6 +4,10 @@ export type Style = CSSObject;
 
 export type Styles = Record<string, Style>;
 
-export type StylesCreatorFunction<P = undefined> = (theme: Theme, props: P) => Styles;
+export type StylesCreatorFunction<P = undefined> = (props: P) => Styles;
 
 export type StylesCreator<P = undefined> = StylesCreatorFunction<P> | Styles | false | null | undefined;
+
+export type StylesThemeCreatorFunction<P = undefined> = (theme: Theme, props: P) => Styles;
+
+export type StylesThemeCreator<P = undefined> = StylesThemeCreatorFunction<P> | Styles | false | null | undefined;
--- packages/styles/src/useStyles/useStyles.ts
+++ packages/styles/src/useStyles/useStyles.ts
@@ -1,27 +1,37 @@
 import { useMemo } from 'react';
 
-import { Styles } from '../types';
+import { Styles, StylesCreatorFunction, StylesCreator } from '../types';
 import { STYLES_EMPTY } from '../constants';
 import { mergeThemeStyles } from '../utils/mergeThemeStyles';
 
-const useStyles = (
-  stylesList: [Styles, Styles | false | null | undefined],
+const useStyles = <P = undefined>(
+  stylesList: [StylesCreatorFunction<P>, StylesCreator<P>],
+  props: P,
   dependencies: unknown[]
 ): Styles => {
-  const [baseStyles, userStyles] = stylesList;
+  const [createBaseStyles, createUserStyles] = stylesList;
 
-  const userStylesType = typeof userStyles === 'boolean' ? 'boolean' : typeof (userStyles || undefined);
+  const userStylesType = typeof createUserStyles === 'boolean'
+    ? 'boolean'
+    : typeof (createUserStyles || undefined);
 
   return useMemo(() => {
-    if (typeof userStyles === 'boolean') {
+    if (typeof createUserStyles === 'boolean') {
       return STYLES_EMPTY;
     }
 
-    if (!userStyles) {
-      return baseStyles;
+    const baseStyles = createBaseStyles(props);
+
+    if (typeof createUserStyles === 'function') {
+      const userStyles = createUserStyles(props);
+      return mergeThemeStyles(baseStyles, userStyles);
     }
 
-    return mergeThemeStyles(baseStyles, userStyles);
+    if (typeof createUserStyles === 'object' && createUserStyles !== null) {
+      return mergeThemeStyles(baseStyles, createUserStyles);
+    }
+
+    return baseStyles;
   }, [userStylesType, ...dependencies]);
 };
 
