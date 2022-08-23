391103fe326524c78a77350b6876a40178a7c1eb
Romel Perez <ronelprhone@gmail.com>
2022-08-22 23:45:33 -0500

    refactor(styles): remove deprecated modules

--- packages/styles/__deprecated__/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './constants';
-
-export * from './createThemePaletteBasic';
-export * from './createThemePaletteElevation';
-export * from './updateColorContrastLuminance';
-export * from './createThemeFactorMultiplier';
-
-export * from './createTheme';
