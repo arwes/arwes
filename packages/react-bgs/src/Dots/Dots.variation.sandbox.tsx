117f878395fce53f7633ca44d2c80a383a0f050d
Romel Perez <ronelprhone@gmail.com>
2022-10-22 20:44:29 -0500

    docs(react-bgs): fix Dots sandbox

--- packages/react-bgs/src/Dots/Dots.variation.sandbox.tsx
+++ packages/react-bgs/src/Dots/Dots.variation.sandbox.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement, useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Animator } from '@arwes/animator';
-import { Dots } from '@arwes/bgs';
+import { Animator } from '@arwes/react-animator';
+import { Dots } from '@arwes/react-bgs';
 
 const Sandbox = (): ReactElement => {
   const [active, setActive] = useState(true);
